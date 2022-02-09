8b9c0570731191d23d5d652d2f45f7284639f85a
rnoguer22 <rnogugon@myuax.com>
2022-02-09 18:22:11 +0100

    Bucles para introducir el valor del lado y la altura

--- Ej10_Area_Triangulo.py
+++ Ej10_Area_Triangulo.py
@@ -1,2 +1,22 @@
 def area(lado, h):
     return (lado*h)/2
+
+while True:
+    lado = input("Introduzca lo que mide el lado: ")
+    try:
+        lado = float(lado)
+        lado > 0
+        break
+    except:
+        pass
+
+while True:
+    h = input("Introduzca lo que mide la altura: ")
+    try:
+        h = float(h)
+        h > 0
+        break
+    except:
+        pass
+
+print ("El area del triangulo es {}".format(area(lado, h)))
