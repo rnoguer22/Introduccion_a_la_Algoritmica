0b4fbf9d430a2af0bd099e4417781378717b67f3
rnoguer22 <rnogugon@myuax.com>
2022-02-09 17:35:37 +0100

    Añadimos la segunda parte del ejercicio De esta manera conseguimos la media ponderada a nuestro cirterio

--- Ej9_Media_Aritmetica_Ponderada.py
+++ Ej9_Media_Aritmetica_Ponderada.py
@@ -1,6 +1,12 @@
 def media(num1, num2, num3):
     media_aritmetica = (num1+num2+num3)/3
+    
+    def media_ponderada(media_aritmetica):
+        #Vamos a redondear a las unidades, a partir del 0.5
+        return round(media_aritmetica, 0)
+
     print ("La media de {}, {}, {} es {}".format(num1, num2, num3, media_aritmetica))
+    print ("La media ponderada es: {}".format(media_ponderada(media_aritmetica)))
 
 def pedir_numero():
     while True:
@@ -9,6 +15,7 @@ def pedir_numero():
             numero = float(numero)
             break
         except:
+            print ("Introduzca un valor correcto por favor")
             pass
     return numero
 
