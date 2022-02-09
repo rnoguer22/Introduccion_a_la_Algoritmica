08c584745c8970ed694616fcb45c5b641acdbc10
rnoguer22 <rnogugon@myuax.com>
2022-02-09 16:41:26 +0100

    Bucle para la correcta introduccion del precio

--- Ej8_Porcentajes_IVA_inversiones.py
+++ Ej8_Porcentajes_IVA_inversiones.py
@@ -1,3 +1,11 @@
 def calcular_precio(precio):
     precio *= 1.21
     return precio
+
+while True:
+    precio = input("Introduzca el valor del precio: ")
+    try:
+        float(precio) == True
+        break
+    except:
+        pass
