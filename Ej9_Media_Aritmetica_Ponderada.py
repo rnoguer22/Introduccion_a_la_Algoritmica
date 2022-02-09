0f2aee548f196dc6b316a2cc0a47b5f6993aaefa
rnoguer22 <rnogugon@myuax.com>
2022-02-09 17:12:25 +0100

    Definimos la funcion "pedir_numero"

--- Ej9_Media_Aritmetica_Ponderada.py
+++ Ej9_Media_Aritmetica_Ponderada.py
@@ -1,2 +1,12 @@
 def media(num1, num2, num3):
     return (sum(num1, num2, num3)/3)
+
+def pedir_numero():
+    while True:
+        numero = input("Introduzca un numero: ")
+        try:
+            numero = float(numero)
+            break
+        except:
+            pass
+    return numero
