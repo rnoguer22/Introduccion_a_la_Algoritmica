58767a85862a02c42ec19898a9cc3323a5c366c4
rnoguer22 <rnogugon@myuax.com>
2022-02-11 19:09:48 +0100

    Solucion errores

--- Ej8_Porcentajes_IVA_inversiones.py
+++ Ej8_Porcentajes_IVA_inversiones.py
@@ -1,33 +1,29 @@
-from cmath import inf
-
-
 class Prima_Anual:
     def __init__(self, antiguedad, distancia, accidentes):
         self.antiguedad = antiguedad
         self.distancia = distancia
         self.accidentes = accidentes
-    
-    def calcular_prima():
 
-        #Funcion para calcular el impuesto asociado a la antiguedad
-        def prima_antiguedad (self, antiguedad):
-            if antiguedad < 4:
-                return 0
-            else:
-                return (200 + 20*(antiguedad-4))
+    #Funcion para calcular el impuesto asociado a la antiguedad
+    def prima_antiguedad(self):
+        if self.antiguedad < 4:
+            return 0
+        else:
+            return (200 + 20*(self.antiguedad-4))
         
-        #Funcion que calcula el impuesto de la distancia
-        def prima_distancia(self, distancia):
-            return inf(0.01*distancia, 400)
+    #Funcion que calcula el impuesto de la distancia
+    def prima_distancia(self):
+        return min(self.distancia*0.01, 400)
         
-        #Funcion que nos calcula la prima ponderada
-        def ponderacion(self, accidentes):
-            prima = prima_antiguedad(self, antiguedad) + prima_distancia(self, distancia)
-            if accidentes < 3:
-                prima /= (accidentes+1)
-            else:
-                prima = 0
-            return prima
+    #Funcion que nos calcula la prima ponderada
+    def ponderacion(self, prima_antiguedad, prima_distancia):
+        prima = prima_antiguedad + prima_distancia
+        if self.accidentes < 3:
+            prima /= (self.accidentes+1)
+        else:
+            prima = 0
+        return prima        
+
 
 #Funcion para pedir los datos al usuario
 def pedir_datos(valor):
@@ -43,9 +39,10 @@ def pedir_datos(valor):
 
 if __name__ == '__main__':
 
+    #Pedimos al usuario los datos
     accidentes = pedir_datos("accidentes")
     distancia = pedir_datos("distancia")
     antiguedad = pedir_datos("antiguedad")
 
 resultado = Prima_Anual(accidentes=accidentes, distancia=distancia, antiguedad=antiguedad)
-print (resultado)
+print (resultado.ponderacion(resultado.prima_antiguedad(), resultado.prima_distancia()))
