2bd0db9108e44c43f103bf8dc0a1e09a9c0af70c
rnoguer22 <rnogugon@myuax.com>
2022-02-11 17:32:36 +0100

    Definimos la clase prima_anual

--- Ej8_Porcentajes_IVA_inversiones.py
+++ Ej8_Porcentajes_IVA_inversiones.py
@@ -1,15 +1,5 @@
-def calcular_precio(precio):
-    precio *= 1.21
-    return precio
-
-while True:
-    precio = input("Introduzca el valor del precio: ")
-    try:
-        precio = float(precio)
-        break
-    except:
-        pass
-
-print ("Precio original: {}. Precio con IVA: {}".format(precio, calcular_precio(precio)))
-
-Falta el apartado 2 !!!
+class prima_anual:
+    def __init__(self, antiguedad, distancia, accidentes):
+        self.antiguedad = antiguedad
+        self.distancia = distancia
+        self.accidentes = accidentes
