3e77c684df305402328e956a8ac8211961ad2be9
rnoguer22 <rnogugon@myuax.com>
2022-02-11 17:34:09 +0100

    Constructor y la funcion "calcular_prima"

--- Ej8_Porcentajes_IVA_inversiones.py
+++ Ej8_Porcentajes_IVA_inversiones.py
@@ -3,3 +3,11 @@ class prima_anual:
         self.antiguedad = antiguedad
         self.distancia = distancia
         self.accidentes = accidentes
+    
+    def calcular_prima():
+
+        def prima_antiguedad (self, antiguedad):
+            if antiguedad < 4:
+                return 0
+            else:
+                return (200 + 20*(antiguedad-4))
