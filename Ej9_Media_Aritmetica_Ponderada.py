a2afc1e2f87b6b41061d6424ce3c9662091b055f
rnoguer22 <rnogugon@myuax.com>
2022-02-12 19:25:55 +0100

    Definimos la funcion "media_no_ponderada" Esta funcion nos calcula la media sin ponderar

--- Ej9_Media_Aritmetica_Ponderada.py
+++ Ej9_Media_Aritmetica_Ponderada.py
@@ -1,5 +1,10 @@
-class Media:
+class Media: 
+    #Constructor
     def __init__(self, num1, num2, num3):
         self.num1 = num1
         self.num2 = num2
         self.num3 = num3
+    
+    #Funcion para calcular la media
+    def media_no_ponderada(self):
+        return (sum(self.num1, self.num2, self.num3))/3
