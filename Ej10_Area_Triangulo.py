e23219fc3f0bbc6f19986ce906edb79242624041
rnoguer22 <rnogugon@myuax.com>
2022-02-12 19:54:50 +0100

    Creamos la clase "Triangulo" asi como su constructor

--- Ej10_Area_Triangulo.py
+++ Ej10_Area_Triangulo.py
@@ -1,22 +1,6 @@
-def area(lado, h):
-    return (lado*h)/2
-
-while True:
-    lado = input("Introduzca lo que mide el lado: ")
-    try:
-        lado = float(lado)
-        lado > 0
-        break
-    except:
-        pass
-
-while True:
-    h = input("Introduzca lo que mide la altura: ")
-    try:
-        h = float(h)
-        h > 0
-        break
-    except:
-        pass
-
-print ("El area del triangulo es {}".format(area(lado, h)))
+class Triangulo:
+    #Constructor
+    def __init__(self, lado, altura):
+        self.lado = lado
+        self.altura = altura
+    
