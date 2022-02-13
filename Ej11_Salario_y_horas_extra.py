45b2021391e6efb9c6af09cfeac4fd226e3e58c7
rnoguer22 <rnogugon@myuax.com>
2022-02-13 17:13:34 +0100

    Funcion "precio_hora_bruto" Calcula el salario bruto por hora

--- Ej11_Salario_y_horas_extra.py
+++ Ej11_Salario_y_horas_extra.py
@@ -2,10 +2,14 @@
 #125% si 36<=x<=43
 #150% si x>43
 #Horas por semana (sin horas extra) = 35; 35x4 = 140 horas al mes
-#Resultado = salario mensual bruto x 12 / (horas por semana x52)
+#Precio por hora bruto = salario mensual bruto x 12 / (horas por semana x52)
 
 class Horas_Extra:
     #Constructor
     def __init__(self, sal_men_bruto, horas_semana):
         self.sal_men_bruto = sal_men_bruto
         self.horas_semana = horas_semana
+    
+    #Funcion que nos calcula el salario por hora
+    def precio_hora_bruto (self):
+        return (self.sal_men_bruto*12)/(self.horas_semana*52)
