7c009aa7e2261180a489a39cf24a9d7e6e917271
rnoguer22 <rnogugon@myuax.com>
2022-02-13 18:12:32 +0100

    Funcion "precio_hora_remunerada" Esta funcion nos devuelve el salario por hora remunerado

--- Ej11_Salario_y_horas_extra.py
+++ Ej11_Salario_y_horas_extra.py
@@ -4,17 +4,15 @@
 #Precio por hora bruto = salario mensual bruto x 12 / (horas por semana x52)
 
 #Funcion para calcular las horas extra de cada tipo
-def precio_horas_remuneradas(horas_semana):
-    r = []   #En esta lista se almacenaran las horas extra de cada tipo
+def horas_remuneradas(horas_semana):
     if 36 <= horas_semana <= 43:
         horas_extra_1 = horas_semana - 35
         horas_extra_2 = 0
     else:
         horas_extra_1 = 8
-        horas_extra_2 = 43 - horas_semana
-    #Añadimos los resultados independientes a la lista
-    r.append(horas_extra_1, horas_extra_2)   
-    return r
+        horas_extra_2 = horas_semana - 43
+    #La funcion devuelve una lista con el numero de horas de ambos tipos
+    return [horas_extra_1, horas_extra_2]
 
 class Horas_Extra:
     #Constructor
@@ -26,8 +24,12 @@ class Horas_Extra:
     def precio_hora_bruto(self):
         return (self.sal_men_bruto*12)/(self.horas_semana*52)
     
-    def precio_hora_remunerada(self, horas_ext, horas_125, horas_150):
-        horas_ext = self.horas_semana - 35
+    #Funcion que devuelve el salario por hora remunerado
+    def precio_hora_remunerada(self):
+        horas_125 = horas_remuneradas()[0]
+        horas_150 = horas_remuneradas()[1]
+        precio_final = self.precio_hora_bruto() * (horas_125*1.25 + horas_150*1.50)
+        return precio_final
 
 #Funcion para introducir las horas por semana por teclado
 def introducir_horas_semana():
